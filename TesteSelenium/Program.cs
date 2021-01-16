78b72d0bd2fba1dc794395371a444c5639a2639f
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-16 18:57:47 -0300

    Teste
    Teste

---
+++ TesteSelenium/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace TesteSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando o uso de Selenium..");
+
+            var firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AddArgument("--headless");
+
+            FirefoxDriver driver;
+
+            if (Environment.OSVersion.VersionString.ToLower().Contains("windows"))
+                driver = new FirefoxDriver("D:\\Selenium\\Firefox\\", firefoxOptions);
+                //driver = new FirefoxDriver(firefoxOptions);
+            else
+                driver = new FirefoxDriver(firefoxOptions);
+
+
+            var siteTestes = Environment.GetEnvironmentVariable("SiteTestes");
+            driver.Navigate().GoToUrl("https://github.com/");
+
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine($"Resultado: {driver.Title}");
+
+            //Close the browser
+            driver.Quit();
+        }
+    }
+}
